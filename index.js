33a3766ff1d2428dbea187a4493257ff2ed0c39b
Evan Lee <evan.lee.615@gmail.com>
2024-03-27 06:00:37 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+
+function createEmployeeRecord (array) {
+    let testEmployee = {
+        firstName: '',
+        familyName: '',
+        title: '',
+        payPerHour: ''
+    }
+    testEmployee.firstName = array[0]
+    testEmployee.familyName = array[1]
+    testEmployee.title = array[2]
+    testEmployee.payPerHour = array[3]
+
+    testEmployee.timeInEvents = []
+    testEmployee.timeOutEvents = []
+
+    return testEmployee
+}
+
+createEmployeeRecord()
+
+
+function createEmployeeRecords (array1, array2) {
+    let employeeRecords = [[array1],[array2]]
+    console.log(employeeRecords.length)
+}
+
+createEmployeeRecords()
+
+
